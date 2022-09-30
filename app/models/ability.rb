b7c245578a0ca47aab321f37ba060501ee3be1bd
Alfred Boateng <programmingwithalfred@gmail.com>
2022-09-30 22:01:23 +0000

    Restrict user access to pages based on roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,5 +13,7 @@ class Ability
     can [:create, :read, :update], Tax if user.collector? || user.statistics? || user.procurement?
     can [:create, :read, :update], StoreTax if user.collector? || user.statistics? || user.procurement?
     can [:create, :read, :update], Payment if user.collector? || user.statistics? || user.procurement?
+    cannot :destroy,
+      :all if user.collector? || user.statistics? || user.procurement? || user.admin? || user.super_admin? || user.it?
   end
 end
