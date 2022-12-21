e295093135eb6bbaf34d36104b8abb9ad09993a4
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-21 09:56:41 +0000

    paginate search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,6 @@
 class SearchController < ApplicationController
   def index
     @query = Store.includes(:user, :assembly, :sub_district, :community, :sector).ransack(params[:q])
-    @stores = @query.result(distinct: true)
+    @stores = @query.result(distinct: true).paginate(page: params[:page], per_page: 8)
   end
 end
